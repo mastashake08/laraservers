049d37ebeb75f443914141f4526724bf20534d29
Jyrone Parker <jyrone.parker@gmail.com>
2018-03-03 19:03:33 -0500

    domain controller logic

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -3,17 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Domain;
+use GuzzleHttp\Client;
 class DomainController extends Controller
 {
+  private $client;
+
+    public function __construct(){
+      $this->client = new Client();
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //
+        return response()->json($request->user()->domains);
     }
 
     /**
@@ -34,7 +41,26 @@ class DomainController extends Controller
      */
     public function store(Request $request)
     {
+
         //
+        $request->validate([
+            'domain' => 'required',
+            'server_id' => 'required',
+        ]);
+        $data = [
+          'domain' => $request->domain,
+          'project_type' => 'php',
+          'directory' =>'/public'
+        ];
+        $site = $this->forge->createSite($request->server_id, $data, $wait = true);
+        $domain = $request->user()->domains()->create([
+          'domain' => $data['domain'],
+          'site_id' => $site->id,
+          'server_id' => $request->server_id
+        ]);
+        return response()->json($domain);
+
+
     }
 
     /**
@@ -46,6 +72,7 @@ class DomainController extends Controller
     public function show($id)
     {
         //
+        return response()->json(Domain::findOrFail($id));
     }
 
     /**
@@ -80,5 +107,10 @@ class DomainController extends Controller
     public function destroy($id)
     {
         //
+        $domain = Domain::findOrFail($id);
+        $this->forge->deleteSite($domain->server_id, $domain->site_id);
+        return response()->json([
+          'success' => $domain->delete()
+        ]);
     }
 }
